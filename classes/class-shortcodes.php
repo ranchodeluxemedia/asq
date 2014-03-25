7da98aabce29cfb08da0bf1b61155d5b03669ea1
Gizburdt <gizburdt@gmail.com>
2014-03-25 10:50:16 +0100

    Overview

--- classes/class-shortcodes.php
+++ classes/class-shortcodes.php
@@ -7,7 +7,8 @@ class Asq_Shortcodes
 	function __construct()
 	{
 		add_shortcode( 'faq', array( &$this, 'faq' ) );
-		add_shortcode( 'faq-contents', array( &$this, 'faq_contents' ) );
+		add_shortcode( 'faq-overview', array( &$this, 'faq_overview' ) );
+		add_shortcode( 'faq-category-list', array( &$this, 'faq_category_list' ) );
 	}
 
 	function faq( $atts, $content = null )
@@ -16,6 +17,9 @@ class Asq_Shortcodes
 			'class'				=> '',
 			'count'				=> -1,
 			'category'			=> ''
+			'show_title'		=> false,
+			'before_title'		=> '<h3>',
+			'after_title'		=> '</h3>'
 		), $atts ) );
 
 		$args		= array( 'post_type' => 'asq_question' );
@@ -35,11 +39,15 @@ class Asq_Shortcodes
 
 		$args['posts_per_page'] = ! empty( $count ) ? $count : -1;
 
-		$query = new WP_Query( $args );
+		$query 		= new WP_Query( $args );
+		$category 	= get_term_by( 'slug', $category, 'asq_category' );
 
 		ob_start();
 
 		echo '<div class="asq ' . $class . '">';
+			if( $show_title )
+				echo $before_title . $category->name . $after_title;
+
 			do_action( 'asq_before_accordion', $class );
 			if( $query->have_posts() ) : 
 				echo '<div class="asq-accordion js-asq-accordion">';
@@ -56,7 +64,7 @@ class Asq_Shortcodes
 		$output = ob_get_clean(); return $output;
 	}
 
-	function faq_contents( $atts, $content = null )
+	function faq_overview( $atts, $content = null )
 	{
 		extract( shortcode_atts( array(
 			'class'				=> '',
@@ -71,7 +79,29 @@ class Asq_Shortcodes
 
 		ob_start();
 
-		echo '<div class="asq-categories ' . $class . '">';
+		foreach( $terms as $term )
+			echo do_shortcode('[faq category="' . $category->slug . '" show_title=true]');
+		endforeach;
+
+		$output = ob_get_clean(); return $output;
+	}
+
+	function faq_category_list( $atts, $content = null )
+	{
+		extract( shortcode_atts( array(
+			'class'				=> '',
+			'orderby'			=> 'name',
+			'order'				=> 'ASC'
+		), $atts ) );
+
+		$terms = get_terms( 'asq_category', array(
+			'orderby'			=> $orderby,
+			'order'				=> $order
+		));
+
+		ob_start();
+
+		echo '<div class="asq-category-list ' . $class . '">';
 			do_action( 'asq_before_category_list', $class );
 			if( ! empty( $terms ) ) : 
 				echo '<ul>';
